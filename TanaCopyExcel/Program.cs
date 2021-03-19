bd1ec5b29681a46b977769b3cbd0a17ccbda31e8
Jan Dryk <me@uxsoft.cz>
2021-03-19 22:19:52 +0100

    Initial commit

---
+++ TanaCopyExcel/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TanaCopyExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
