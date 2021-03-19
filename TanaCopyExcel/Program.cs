572d31465769732b96b26fc3fb50f1259e0502dd
Jan Dryk <me@uxsoft.cz>
2021-03-19 22:21:36 +0100

    implemented

--- TanaCopyExcel/Program.cs
+++ TanaCopyExcel/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.IO;
 
-namespace TanaCopyExcel
+var destination = @"C:\Users\uxsoft\Desktop\";
+var template = @"C:\Users\uxsoft\Desktop\template.xlsx";
+var names = @"
+asd
+asd1
+asd2
+asd3
+asd4
+";
+
+Console.WriteLine($"Using template: {template}");
+
+foreach (var name in names.Split(new[] {'\r', '\n'}, StringSplitOptions.RemoveEmptyEntries))
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+    var fileName = Path.Combine(destination, $"{name.Trim()}.xlsx");
+    File.Copy(template, fileName);
+    Console.WriteLine($"Created {fileName}");
 }
+
+Console.WriteLine("Finished creating files!");
+Console.ReadKey();
